68908f93632c925f749252facdf5350f916ba8c9
paschdan <paschdan@gmail.com>
2020-02-03 10:04:49 +0100

    use alternative history settings

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,18 +1,23 @@
 if (( WSL )); then
   typeset -g HISTFILE=~/.zsh_history.${(%):-%m}-wsl-${HOME:t}
 else
-  typeset -g HISTFILE=~/.zsh_history.${(%):-%m}-linux-${HOME:t}
+  if [ "${TMUX}" ] ; then
+    typeset -g HISTFILE=~/.zsh_history.$(tmux display-message -p  -t$TMUX_PANE "#{window_name}")
+  else
+    typeset -g HISTFILE=~/.zsh_history.${(%):-%m}-linux-${HOME:t}
+  fi
 fi
 
 typeset -gi HISTSIZE=1000000000
 typeset -gi SAVEHIST=1000000000
 typeset -gi HISTFILESIZE=1000000000
 
-() {
-  emulate -L zsh
-  setopt extended_glob
-  local hist
-  for hist in ~/.zsh_history*~$HISTFILE(N); do
-    fc -RI $hist
-  done
-}
+# Appends every command to the history file once it is executed
+setopt inc_append_history
+# Reloads the history whenever you use it
+setopt share_history
+
+#() {
+#  emulate -L zsh
+#  fc -RI $HISTFILE
+#}
