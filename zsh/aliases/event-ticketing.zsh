70c1fbc8c4d89fda772ec83751bbb4b0e0b70406
paschdan <paschdan@gmail.com>
2020-02-07 13:27:24 +0100

    add more steps to restart of et

--- zsh/aliases/event-ticketing.zsh
+++ zsh/aliases/event-ticketing.zsh
@@ -1,8 +1,24 @@
 function restart_et {
-  docker-compose down -v
+  docker-compose down -v --remove-orphans
+  docker-compose -f docker-compose.tools.yml run --rm composer install 
+  docker-compose -f docker-compose.tools.yml run --rm node npm i
+  docker-compose -f docker-compose.tools.yml run --rm node npm run build:parallel 
   docker-compose up -d
   docker-compose stop cycler
   docker-compose logs -f event-ticketing forwarder
+}
+
+function et_build_docker {
+  VERSION=${@}
+  if [ VERSION = "" ]; then
+    echo "NO VERSION GIVEN TO BUILD"
+    return 2
+  fi
+  if [ ! -d ${VERSION} ]; then
+    echo "MAKE SURE TO BE IN THE docker-event-ticketing FOLDER AND THE VERSION IS EXISTING"
+    return 2
+  fi
+  docker build --tag docker.wdf.sap.corp:50002/event-ticketing/event-ticketing-${VERSION}-snapshot ${VERSION}
 }
 
 alias etr='restart_et'
